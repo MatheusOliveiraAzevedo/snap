2de2b6dc9299f28bb1bc3814f71e9288afc30357
MatheusOliveiraAzevedo <89955024+MatheusOliveiraAzevedo@users.noreply.github.com>
2023-08-09 09:15:14 -0300

    Update index.jsx

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -25,6 +25,14 @@ const Cabecalho = () => {
     }
     
     useEffect(() => {
+        
+        let larguraTela = window.innerWidth;
+        if (larguraTela <= 1024) {
+            setMenuMobile(false)
+        } else {
+            setMenuMobile(true)
+        }
+        
         const tamanhoTela = () => {
             if (window.matchMedia("(max-width: 1024px)").matches) {
                 setMenuMobile(false)
